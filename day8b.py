f96e8dc06e6789f0efc8a9931291a61f3bf2b61f
Andrew <sssss465@gmail.com>
2019-12-08 06:03:46 -0800

    8b fast

--- day8b.py
+++ day8b.py
@@ -5,30 +5,28 @@ for line in sys.stdin:
     pic = [int(i) for i in list(line.rstrip())]
 width = 25
 length = 6
-
 assert(len(pic) % (width // length) == 0)
-
-res = [[[0] * width for i in range(length)]
-       for j in range(len(pic) // (width*length))]
-p = 0
-layer = None
-min_zero = float('inf')
-for l in range(len(pic) // (width*length)):
-    zeros = 0
-    for i in range(length):
-        for j in range(width):
-            if pic[p] == 0:
-                zeros += 1
-            res[l][i][j] = pic[p]
-            p += 1
-    if zeros < min_zero:
-        min_zero = zeros
-        print(min_zero)
-        layer = l
-l = layer
+res = np.reshape(pic, (100, 6, 25))  # cheat
+# res = [[[0] * width for i in range(length)]
+#        for j in range(len(pic) // (width*length))]
+# p = 0
+# layer = None
+# min_zero = float('inf')
+# for l in range(len(pic) // (width*length)):
+#     zeros = 0
+#     for i in range(length):
+#         for j in range(width):
+#             if pic[p] == 0:
+#                 zeros += 1
+#             res[l][i][j] = pic[p]
+#             p += 1
+#     if zeros < min_zero:
+#         min_zero = zeros
+#         layer = l
+# l = layer
 # print(sum([r.count(1) for r in res[l]]) * sum([r.count(2) for r in res[l]]))
 
-res = np.array(res)  # tofix
+# res = np.array(res)  # tofix
 # x = np.argmax(res != 2, axis=0).flatten()
 # print(np.shape(x))
 
