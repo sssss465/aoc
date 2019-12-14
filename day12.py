f9453fbfcf1ea09b0ed15206a5234976eb447916
Andrew <sssss465@gmail.com>
2019-12-14 12:39:28 -0800

    12-2

--- day12.py
+++ day12.py
@@ -1,6 +1,7 @@
+import functools
 import sys
 import numpy as np
-
+import copy
 moons = []
 
 
@@ -29,19 +30,56 @@ class Moon:
 for line in sys.stdin:
     line = line.rstrip('>\n').lstrip('<').split(',')
     line = [int(l.split('=')[1]) for l in line]
-    print(line)
     moons.append(Moon(*line))
 
-steps = 1
-for i in range(0, steps+1):
-    print('step: ', i)
-    for m in moons:
-        print(m)
-    if i == steps:
-        print(sum([m.energy() for m in moons]))
+steps = 0
+i = 0
+start = copy.deepcopy(moons)
 
+moonflag = [False] * 3
+res = [0] * 3
+
+
+def gcd(a, b):
+    """Compute the greatest common divisor of a and b"""
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    """Compute the lowest common multiple of a and b"""
+    return a * b // gcd(a, b)
+
+
+while True:
+    # print('step: ', i, moonflag, res)
+    # for m in moons:
+    #     print(m)
+    if i > 0:
+        if all(moonflag):
+            break
+        for j in range(3):
+            if res[j] == 0:
+                flag = True
+                for m in range(len(moons)):
+                    if moons[m].pos[j] == start[m].pos[j] and moons[m].vel[j] == 0:
+                        continue  # print(moons[m].pos[j], moons[m].vel[j])
+                    else:
+                        flag = False
+                        break
+                if flag:
+                    moonflag[j] = True
+                    res[j] = i
     for m in moons:
         for n in moons:
-            m.gravity(n)
+            if m is not n:
+                m.gravity(n)
     for m in moons:
         m.velocity()
+    i += 1
+print(res)
+r = res[0]
+for i in range(1, len(res)):
+    r = lcm(r, res[i])
+print('answer is', r)
