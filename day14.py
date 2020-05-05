cf8e4132b0dbe18e291d7b689c9b6d501561fc7d
Andrew <sssss465@gmail.com>
2020-05-05 00:24:23 -0700

    thanks sheks

--- day14.py
+++ day14.py
@@ -1,36 +1,94 @@
+# https://adventofcode.com/2019/day/14
+import math
+import collections as cl
 import sys
-import collections
 
-graph = collections.defaultdict(dict)
+from collections import deque
 
-total = collections.defaultdict(int)
+graph = cl.defaultdict(list)
+total = cl.defaultdict(int)
+total_fuel = 0
+for Line in sys.stdin:
+    # store each pair
+    Mats, Recp = [[[int(Pr.split(' ')[0]), Pr.split(' ')[1]] for Pr in
+                   Sd.strip().split(", ")] for Sd in Line.split('=>')]
+    for quantity_recv, elem_recv in Recp:
+        total[elem_recv] = 0
+        if elem_recv == "FUEL":
+            total_fuel = quantity_recv
+        graph[elem_recv] = [quantity_recv, []]
+        for quantity_mat, elem_mat in Mats:
+            graph[elem_recv][1].append([quantity_mat, elem_mat])
+# print(graph, total)
+r = cl.defaultdict(int)
+have = cl.defaultdict(int)
+# def bfs(graph, total_fuel):
+#     q = deque()
+#     q.append(graph['FUEL'])
+#     res = 0
+#     while len(q) > 0:
+#         cur_quant, curproducts = q.popleft()
+#         print(q, "     ",  curproducts)
+#         for quant, prod in curproducts:
+#             if prod == 'ORE':
+#                 continue
+#             if graph[prod][1][0][1] == 'ORE':
+#                 r[prod] += cur_quant*quant
+#             q.append([cur_quant * quant, graph[prod][1]])
+#     return res
 
-for line in sys.stdin:
-    line = line.strip().split('=>')
-    l1 = line[0].strip().split(',')
-    l2 = line[1].strip().split(',')
-    for g in l2:
-        g = g.strip().split(' ')
-        total[g[1]] = 0
-        for l in l1:
-            l = l.strip().split(' ')
-            print(l)
-            graph[g[1]][l[1]] = int(l[0])
-print(graph)
-print(total)
+
+def Simulate(Quantity):
+    Tree = cl.defaultdict(int, {'FUEL': Quantity})
+    while not all(Pt == "ORE" or Tree[Pt] <= 0 for Pt in Tree):
+        for Material, Quantity in Tree.copy().items():
+            # print(Material, Quantity, Tree)
+            if Material == "ORE":
+                continue
+            Coeff, Products = graph[Material]
+            rTimes = math.ceil(Quantity / Coeff)
+            for Amnt, Element in Products:
+                Tree[Element] += Amnt * rTimes
+            Tree[Material] -= rTimes * Coeff
+    # print(Tree)
+    return Tree["ORE"]
 
 
-def dfs(node, quantity=1):
-    # print('hello', node, quantity)
-    if node == 'ORE':
-        return quantity
+# res = 0
+# print(bfs(graph, total_fuel))
+# for k, v in r.items():
+#     quant, prods = graph[k]
+#     ore_amt = prods[0][0]
+#     big = math.ceil(v / quant)
+#     print(k, v, big)
+#     res += big * ore_amt
+Steps, Order = cl.defaultdict(set), cl.defaultdict(list)
 
-    material = 0
-    for mat, quant in graph[str(node)].items():
-        material += dfs(mat, quant)
-    # print('returning', material*quantity, material, quantity)
-    return material
+
+def Walk(Pt, n=0):
+    for _, X in graph[Pt][1]:
+        if X in graph:
+            Walk(X, n+1)
+    Steps[Pt].add(n)
 
 
-r = dfs('FUEL', 1)
-print(r)
+Walk("FUEL")
+for k, v in Steps.items():
+    Order[max(v)].append(k)
+low = 10**12 // Simulate(1)
+high = 10*low
+while Simulate(high) < 1e12:
+    low = high
+    high = 10*low
+while low < high:
+    mid = (low+high) // 2
+    ore = Simulate(mid)
+    if ore < 1e12:
+        low = mid+1
+    else:
+        high = mid
+print(low, high)
+print(Simulate(high))
+
+print("Silver:", Simulate(total_fuel))
+print("Gold:", int(high-1))
