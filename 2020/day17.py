3398332089fd25fd3f6b9ce2ed4ffb40b2d1dcc0
Andrew <sssss465@gmail.com>
2021-05-08 05:34:46 -0700

    day17 cache fast

--- 2020/day17.py
+++ 2020/day17.py
@@ -14,11 +14,21 @@ def solve(gold=False):
           row in enumerate(lines) for c_c, col in enumerate(row) if col == '#'}
     # print(mp)
     for round in range(6):
+        nb_cache = {}
+        deg_cache = {}
+
         def nearby(pos):
-            return [(*map(operator.add, pos, off),) for off in itertools.product(offset, repeat=len(pos))][1:]
+            if pos in nb_cache:
+                return nb_cache[pos]
+            nb_cache[pos] = [(*map(operator.add, pos, off),)
+                             for off in itertools.product(offset, repeat=len(pos))][1:]
+            return nb_cache[pos]
 
         def degree(pos):
-            return sum(int(p in mp) for p in nearby(pos))
+            if pos in deg_cache:
+                return deg_cache[pos]
+            deg_cache[pos] = sum(int(p in mp) for p in nearby(pos))
+            return deg_cache[pos]
         # we only need to check active cubes and their neighbors because inactive cubes can only become active from 3 neighbors
         neighbors = [nei for p in mp for nei in nearby(p)]
         add = set(filter(lambda p: degree(p) == 3, neighbors))
