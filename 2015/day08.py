94714ab9fae5289cf76d7d8204dffe30500c39b5
Andrew <sssss465@gmail.com>
2021-10-23 04:47:31 -0700

    day 8 part 2

--- 2015/day08.py
+++ 2015/day08.py
@@ -3,7 +3,9 @@ import fileinput
 lines = [l.strip() for l in fileinput.input()]
 
 silver = 0
-
+gold = 0
 for line in lines:
     silver += len(line) - len(eval(line))
+    gold += 2 + line.count('\\') + line.count('"')
 print(f"silver: {silver}")
+print(f"gold: {gold}")
