a03871885e2b1643b84a38d3399e4e4c982ea983
nobuu <1025790+sssss465@users.noreply.github.com>
2022-11-30 22:07:03 -0800

    2022 day 01

---
+++ 2022/day01.py
@@ -0,0 +1,17 @@
+import fileinput
+from dataclasses import dataclass, field
+
+lines = [line.strip() for line in fileinput.input()]
+
+arr = []
+cur = 0
+for line in lines:
+    if not line:
+        arr.append(cur)
+        cur = 0
+    else:
+        cur += int(line)
+arr.append(cur)
+
+print(f'🤍 {max(arr):>6}')
+print(f'💛 {sum(sorted(arr)[-3:]):>06}')
