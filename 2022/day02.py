c12d59584651b2ed7a73cedd88cf4301c55217d9
nobuu <1025790+sssss465@users.noreply.github.com>
2022-12-01 22:20:51 -0800

    2022 day 02

---
+++ 2022/day02.py
@@ -0,0 +1,27 @@
+import fileinput
+from collections import defaultdict, Counter
+
+lines = [line.strip() for line in fileinput.input()]
+rock = {'C': 'S', 'Z': "S", 'A': 'R', 'B': 'P',  'X': 'R', 'Y': 'P'}
+beats = {'R': 'S', 'P': 'R', 'S': 'P'}
+loses = {'R': 'P', 'P': 'S', 'S': 'R'}
+pts = {'R': 1, 'P': 2, 'S': 3}
+silver = 0
+gold = 0
+
+for line in lines:
+    l, r = line.split()
+    l = rock[l]
+    r = rock[r]
+    if r == 'R':
+        rg = beats[l]
+    elif r == 'P':
+        rg = l
+    else:
+        rg = loses[l]
+    silver += 6 if beats[r] == l else (l == r) * 3
+    silver += pts[r]
+    gold += 6 if beats[rg] == l else (l == rg) * 3
+    gold += pts[rg]
+print('silver', silver)
+print('gold', gold)
