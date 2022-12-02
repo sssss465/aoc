b2c287cb85ac85f031cc73d5a48f60f1e4cfdc59
nobuu <1025790+sssss465@users.noreply.github.com>
2022-12-02 01:38:24 -0800

    2022 day 02

--- 2022/src/day02.rs
+++ 2022/src/day02.rs
@@ -14,8 +14,7 @@ fn points(l: u8, r: u8) -> u8 {
 pub fn main() {
     let contents = include_str!("../2");
     let lines = contents
-        .split('\n')
-        .filter(|line| !line.is_empty())
+        .lines()
         .map(|set| set.split(' ').collect::<Vec<_>>())
         .collect::<Vec<_>>();
     let nxt = HashMap::from([("A", " CAB"), ("B", " ABC"), ("C", " BCA")]);
@@ -24,8 +23,7 @@ pub fn main() {
     let mut silver: i32 = 0;
     let mut gold: i32 = 0;
     for v in lines {
-        let l = v[0];
-        let r = v[1];
+        let (l, r) = (v[0], v[1]);
         silver += points(score[l], score[r]) as i32;
         gold += points(
             score[l],
