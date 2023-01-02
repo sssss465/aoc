29721f0a4c8a18eda4470d402c8cd3c04a4044c3
nobuu <1025790+sssss465@users.noreply.github.com>
2023-01-01 22:33:32 -0800

    misc stuff 2021 aoc rust

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -1,16 +1,16 @@
 fn main() {
     println!("Hello, world!");
     let mut x = 5;
-    let mut ds = String::from("hello");
+    let mut s = String::from("hello");
     println!("The value of x is: {}", x);
-    test(x, s);
+    //test(x, &s);
     x += 1;
     println!("The value of x is: {}", x);
     s.push_str(" world");
     println!("The value of s is: {}", s);
 }
 
-fn test(parm: i32, s: String) -> Vec<i32> {
+fn test(parm: i32, s: &String) -> Vec<i32> {
     let x = 5;
     let y = {
         let x = 3;
